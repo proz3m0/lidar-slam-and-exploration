b228b00ee83bd63bcecd2110012f86cc584dc898
UTSAnonymous <12480000@student.uts.edu.au>
2019-10-07 00:37:31 +1100

    added maze_explorer algorithm

---
+++ maze_explorer/src/main.cpp
@@ -0,0 +1,25 @@
+#include "ros/ros.h"
+#include "maze_explorer_node.h"
+
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "map_processing");
+
+    ros::NodeHandle nh;
+    
+    MazeExplorer mp(nh);
+
+    /*
+    // steps for executing the algorithm
+    mp.dilateOccupiedSpace(8);
+    mp.findIntersection();
+    mp.showMap();
+    mp.arrangeQueue();
+    mp.showQueuedPoint();
+    */
+
+    ros::spin();
+
+    return 0;
+}
---
+++ maze_explorer/src/maze_explorer_node.h
@@ -0,0 +1,128 @@
+#include <iostream>
+
+#include <tuple>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <mutex>
+
+#include "ros/ros.h"
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+#include <geometry_msgs/PoseArray.h>
+#include <nav_msgs/MapMetaData.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include "maze_explorer/RequestMazePath.h"
+
+const  cv::Vec3b white(255,255,255),
+                 red(0, 0, 255),
+                 blue(255, 0 , 0),
+                 black(0, 0, 0),
+                 grey(127, 127, 127),
+                 green(0, 255, 0);
+
+const int OCCUPIED_THRES = 65;
+
+// row, col
+struct matPoint
+{
+    int row;
+    int col; 
+
+    bool operator==(const matPoint &lhs)
+    {
+        return (lhs.row == row) && (lhs.col == col);
+    };
+};
+// a,b,c (ab = top left corner), c = width and height (square)
+struct IntersectionData
+{
+    int topleftrow;
+    int topleftcol;
+    int size;
+};
+
+class MazeExplorer
+{
+public:
+
+    MazeExplorer(ros::NodeHandle nh);
+    
+    void dilateOccupiedSpace(int amount);
+
+    void showMap();
+    
+    void findIntersection();
+
+    void arrangeQueue();
+
+    void showQueuedPoint();
+
+    geometry_msgs::Pose localToGlobal(matPoint &local);
+
+    matPoint globalToLocal(geometry_msgs::Pose &pose);
+
+    void convertOccupancyGrid();
+
+    std::vector<geometry_msgs::PoseStamped> getIntersectionPose(); 
+
+protected:
+
+    //======================== ROS ========================//
+
+    ros::NodeHandle nh_;
+    ros::Subscriber occupMapSub_;
+    ros::ServiceServer exploreServer_;
+
+    void occupMapCB(const nav_msgs::OccupancyGrid::ConstPtr &data);
+    bool exploreMazeAlgorithm(maze_explorer::RequestMazePath::Request  &req,
+                              maze_explorer::RequestMazePath::Response &res);
+
+    bool checkedMapMetadata_;
+
+    struct MapMetadataStruct
+    {
+        nav_msgs::MapMetaData data;
+        std::mutex mutex;
+    };
+    MapMetadataStruct mapMetadata_;
+
+    struct OccupMapStruct
+    {
+        nav_msgs::OccupancyGrid data;
+        std::mutex mutex;
+    };
+    OccupMapStruct occupMap_;
+
+
+    //======================== Functions ========================//
+    //vertical and horizontal trace
+    std::vector<bool> vhTrace(int traceAmount, int row, int col);
+
+    bool isIntersection(std::vector<bool> input);
+
+    void addIntersectionSquare(int size, matPoint intersectionPoint);
+
+    bool checkIntersectionSqaure(matPoint point);
+
+    std::vector<matPoint> getNeighbour(matPoint point);
+
+    //======================== Variables ========================//
+    cv::Mat map_;
+    matPoint robotStartingPose_;
+    
+    std::vector<matPoint> intersectionpoint_;
+
+    std::vector<IntersectionData> blockedintersection_;
+        
+    std::vector<matPoint> queuedIntersectionpoint_;
+
+};
+
+
